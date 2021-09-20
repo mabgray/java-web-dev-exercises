c154e1d463a2d60070616f06e576c0ef0006022a
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2021-09-20 14:38:38 -0500

    worked with test practice

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -2,6 +2,6 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class SchoolPractice {
     public static void main(String[] args) {
-        Student maryanne = new Student("maryanne", 01,1, 4.0 );
+       // Student maryanne = new Student("maryanne", 01,1, 4.0 );
     }
 }
